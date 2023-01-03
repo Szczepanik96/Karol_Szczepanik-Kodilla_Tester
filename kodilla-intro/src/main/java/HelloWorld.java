6d326ee09dd4996b2048e622afa8b02f20324538
Karol Szczepanik <szczepanik.dope@gmail.com>
2023-01-03 16:38:55 +0100

    initial commit

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args)  {
+        System.out.println("Hello World");
+
+        System.out.println("Karol");
+
+        System.out.println("27");
+
+        System.out.println("S");
+        }
+
+}
---
+++ kodilla-intro/src/main/java/com/kodilla/FirstClass.java
@@ -0,0 +1,7 @@
+package com.kodilla;
+
+public class FirstClass {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello from com.kodilla.FirstClass!"); // [3]
+    }
+}
