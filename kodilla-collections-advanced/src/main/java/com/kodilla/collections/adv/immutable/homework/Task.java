4d9760137a5e0af4745e47a00c8af170ad048f65
Karol Szczepanik <szczepanik.dope@gmail.com>
2023-02-08 14:49:52 +0100

    zadanie 5.3 IMMUTABLE

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
@@ -0,0 +1,8 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class Task {
+    public static void main(String[] args) {
+        TaskRecord task = new TaskRecord("IDK o co w tym zadadniu może więcej chodzić",420);
+        System.out.println(task);
+    }
+}
