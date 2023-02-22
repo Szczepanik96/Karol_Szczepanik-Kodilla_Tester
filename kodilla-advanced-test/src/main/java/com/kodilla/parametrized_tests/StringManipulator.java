5d66773f6e262f51dc0397d5d5f53ee14697c511
Karol Szczepanik <szczepanik.dope@gmail.com>
2023-02-22 19:04:43 +0100

    backUp

--- kodilla-advanced-test/src/main/java/com/kodilla/parametrized_tests/StringManipulator.java
+++ kodilla-advanced-test/src/main/java/com/kodilla/parametrized_tests/StringManipulator.java
@@ -1,16 +1,33 @@
 package com.kodilla.parametrized_tests;
 
+import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.Arguments;
+import java.util.stream.Stream;
+
 public class StringManipulator {
     public String reverseWithLowerCase(String input) {
         StringBuilder builder = new StringBuilder(input);
         return builder.reverse().toString().toLowerCase();
     }
+
     public int getStringLengthWithoutSpaces(String input) {
         String value = input.replaceAll(" ", "");
         return value.length();
     }
+
     public int countNumberOfCommas(String text) {
         int count = text.length() - text.replace(",", "").length();
         return count;
+    }
+
+    private static Stream<Arguments> provideStringsForTestingLength() {
+        return Stream.of(
+                Arguments.of("test", 4),
+                Arguments.of("OtHEr ", 5),
+                Arguments.of("E V e n t", 5),
+                Arguments.of("null ", 4),
+                Arguments.of("A", 1)
+        );
+
     }
 }
--- kodilla-advanced-test/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
+++ kodilla-advanced-test/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
@@ -1,36 +1,27 @@
 package com.kodilla.parametrized_tests.homework;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
+//class GamblingMachineTest {
+//    private final GamblingMachine gamblingMachine = new GamblingMachine();
 
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-class GamblingMachineTest {
-    private final GamblingMachine gamblingMachine = new GamblingMachine();
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/howManyWins.csv", numLinesToSkip = 1, delimiter = ':')
-    public void shouldCountEveryTimeIfUserNumberAndComputerNumberWillBeTheSame(String inputNumber, int expectedOutput) throws InvalidNumbersException {
-        String[] numbersArray = inputNumber.split(",");
-        Set<Integer> userNumbers = new HashSet<>();
-        for (String number : numbersArray) {
-            userNumbers.add(Integer.parseInt(number));
-        }
-        int result = gamblingMachine.howManyWins(userNumbers);
-        assertEquals(expectedOutput,result);
-    }
-    @ParameterizedTest
-    @CsvFileSource(resources = "/InvalidNumbers.csv", numLinesToSkip = 1, delimiter = ':')
-    public void invalidNumbersProvidedShouldThrowInvalidNumbersException(String invalidInput) throws InvalidNumbersException{
-        String[] numbersArray = invalidInput.split(",");
-        Set<Integer> invalidNumbers = new HashSet<>();
-        for (String number:numbersArray){
-            invalidNumbers.add(Integer.parseInt(number));
-        }
-        assertThrows(InvalidNumbersException.class,() -> gamblingMachine.howManyWins(invalidNumbers));
-    }
-}
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/howManyWins.csv", numLinesToSkip = 1, delimiter = ':')
+//    public void shouldCountEveryTimeIfUserNumberAndComputerNumberWillBeTheSame(String inputNumber, int expectedOutput) throws InvalidNumbersException {
+//        String[] numbersArray = inputNumber.split(",");
+//        Set<Integer> userNumbers = new HashSet<>();
+//        for (String number : numbersArray) {
+//            userNumbers.add(Integer.parseInt(number));
+//        }
+//        int result = gamblingMachine.howManyWins(userNumbers);
+//        assertEquals(expectedOutput,result);
+//    }
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/InvalidNumbers.csv", numLinesToSkip = 1, delimiter = ':')
+//    public void invalidNumbersProvidedShouldThrowInvalidNumbersException(String invalidInput) throws InvalidNumbersException{
+//        String[] numbersArray = invalidInput.split(",");
+//        Set<Integer> invalidNumbers = new HashSet<>();
+//        for (String number:numbersArray){
+//            invalidNumbers.add(Integer.parseInt(number));
+//        }
+//        assertThrows(InvalidNumbersException.class,() -> gamblingMachine.howManyWins(invalidNumbers));
+//    }
+//}
